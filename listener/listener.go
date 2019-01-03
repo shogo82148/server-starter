c728a2f515b98f703160608a6a64fc9f888659aa
Ichinose Shogo <shogo82148@gmail.com>
2019-01-03 12:03:49 +0900

    implement the listener package.

---
+++ listener/listener.go
@@ -0,0 +1,155 @@
+package listener
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// ServerStarterEnvVarName is the environment name for server_starter configures.
+const ServerStarterEnvVarName = "SERVER_STARTER_PORT"
+
+// ErrNoListeningTarget is returned by ListenAll calls
+// when the process is not started using server_starter.
+var ErrNoListeningTarget = errors.New("listener: no listening target")
+
+// ListenConfig is the interface for things that listen on file descriptors
+// specified by Start::Server / server_starter
+type ListenConfig interface {
+	// Fd returns the underlying file descriptor
+	Fd() uintptr
+
+	// Listen creates a new Listener
+	Listen() (net.Listener, error)
+
+	// return a string compatible with SERVER_STARTER_PORT
+	String() string
+}
+
+// ListenConfigs holds a list of ListenConfig. This is here just for convenience
+// so that you can do
+//	list.String()
+// to get a string compatible with SERVER_STARTER_PORT
+type ListenConfigs []ListenConfig
+
+func (ll ListenConfigs) String() string {
+	if len(ll) == 0 {
+		return ""
+	}
+
+	var builder strings.Builder
+	for _, l := range ll {
+		builder.WriteString(l.String())
+		builder.WriteByte(';')
+	}
+	s := builder.String()
+	return s[:len(s)-1] // remove last ';'
+}
+
+// Listen announces on the local network address.
+// The network must be "tcp", "tcp4", "tcp6", "unix" or "unixpacket".
+func (ll ListenConfigs) Listen(ctx context.Context, network, address string) (net.Listener, error) {
+	var addrlist []string
+	switch network {
+	case "tcp", "tcp4", "tcp6":
+		host, port, err := net.SplitHostPort(address)
+		if err != nil {
+			return nil, err
+		}
+		portnum, err := net.DefaultResolver.LookupPort(ctx, network, port)
+		if err != nil {
+			return nil, err
+		}
+		port = strconv.Itoa(portnum)
+
+		ips, err := net.DefaultResolver.LookupIPAddr(ctx, host)
+		if err != nil {
+			return nil, err
+		}
+		// if the machine has halfway configured
+		// IPv6 such that it can bind on "::" (IPv6unspecified)
+		// but not connect back to that same address, fall
+		// back to dialing 0.0.0.0.
+		if len(ips) == 1 && ips[0].IP.Equal(net.IPv6unspecified) {
+			ips = append(ips, net.IPAddr{IP: net.IPv4zero})
+		}
+		for _, ip := range ips {
+			addrlist = append(addrlist, net.JoinHostPort(ip.String(), port))
+		}
+	case "unix", "unixpacket":
+		addrlist = []string{address}
+	default:
+		return nil, net.UnknownNetworkError(network)
+	}
+
+	for _, l := range ll {
+		for _, addr := range addrlist {
+			if l.String() == addr {
+				return l.Listen()
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("listener: address %s is not being bound to the server", address)
+}
+
+type listenConfig struct {
+	addr string
+	fd   uintptr
+}
+
+func (l listenConfig) String() string {
+	return fmt.Sprintf("%s=%d", l.addr, l.fd)
+}
+
+func (l listenConfig) Fd() uintptr {
+	return l.fd
+}
+
+func (l listenConfig) Listen() (net.Listener, error) {
+	return net.FileListener(os.NewFile(l.Fd(), l.addr))
+}
+
+func parseListenTargets(str string) ([]ListenConfig, error) {
+	if str == "" {
+		return nil, ErrNoListeningTarget
+	}
+
+	rawspec := strings.Split(str, ";")
+	ret := make([]ListenConfig, len(rawspec))
+
+	for i, pairString := range rawspec {
+		pair := strings.SplitN(pairString, "=", 2)
+		if len(pair) != 2 {
+			return nil, fmt.Errorf("failed to parse '%s' as listen target", pairString)
+		}
+		addr := strings.TrimSpace(pair[0])
+		fdString := strings.TrimSpace(pair[1])
+		fd, err := strconv.ParseUint(fdString, 10, 0)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse '%s' as listen target: %s", pairString, err)
+		}
+		ret[i] = listenConfig{
+			addr: addr,
+			fd:   uintptr(fd),
+		}
+	}
+
+	return ret, nil
+}
+
+// PortsSpecification returns the value of SERVER_STARTER_PORT
+// environment variable.
+// If it is not set, return the empty string.
+func PortsSpecification() string {
+	return os.Getenv(ServerStarterEnvVarName)
+}
+
+// Ports parses environment variable SERVER_STARTER_PORT
+func Ports() (ListenConfigs, error) {
+	return parseListenTargets(PortsSpecification())
+}
---
+++ listener/listener_test.go
@@ -0,0 +1,95 @@
+package listener
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestPort(t *testing.T) {
+	caces := []struct {
+		in string
+		ll []listenConfig
+	}{
+		{
+			in: "0.0.0.0:80=3",
+			ll: []listenConfig{
+				{
+					addr: "0.0.0.0:80",
+					fd:   3,
+				},
+			},
+		},
+		{
+			in: "0.0.0.0:80=3;/tmp/foo.sock=4",
+			ll: []listenConfig{
+				{
+					addr: "0.0.0.0:80",
+					fd:   3,
+				},
+				{
+					addr: "/tmp/foo.sock",
+					fd:   4,
+				},
+			},
+		},
+		{
+			in: "50908=4",
+			ll: []listenConfig{
+				{
+					addr: "50908",
+					fd:   4,
+				},
+			},
+		},
+	}
+
+	for i, tc := range caces {
+		os.Setenv(ServerStarterEnvVarName, tc.in)
+		ll, err := Ports()
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if len(ll) != len(tc.ll) {
+			t.Errorf("#%d: want %d, got %d", i, len(tc.ll), len(ll))
+		}
+		for i, l := range ll {
+			l := l.(listenConfig)
+			if !reflect.DeepEqual(l, tc.ll[i]) {
+				t.Errorf("#%d, want %#v, got %#v", i, tc.ll[i], l)
+			}
+		}
+		if ll.String() != tc.in {
+			t.Errorf("#%d, want %s, got %s", i, tc.in, ll.String())
+		}
+	}
+
+	errs := []string{
+		"0.0.0.0:80=foo", // invalid fd
+		"0.0.0.0:80",     // missing fd
+	}
+	for i, tc := range errs {
+		os.Setenv(ServerStarterEnvVarName, tc)
+		ll, err := Ports()
+		if err == nil {
+			t.Errorf("#%d: want error, got nil", i)
+		}
+		if ll != nil {
+			t.Errorf("#%d: want nil, got %#v", i, ll)
+		}
+	}
+}
+
+func TestPortNoEnv(t *testing.T) {
+	os.Setenv(ServerStarterEnvVarName, "")
+
+	ports, err := Ports()
+	if err != ErrNoListeningTarget {
+		t.Error("Ports must return error if no env")
+	}
+
+	if ports != nil {
+		t.Errorf("Ports must return nil if no env")
+	}
+}
