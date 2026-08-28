d480c814bcbc910a294980a3b336901c6ed58121
ICHINOSE Shogo <shogo82148@gmail.com>
2026-08-29 08:57:53 +0900

    add cmd logger implementation and tests for Logf and Shutdown methods

--- logger_test.go
+++ logger_test.go
@@ -2,10 +2,15 @@ package starter
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
+	"time"
 )
 
 func captureStderr(t *testing.T, f func()) []byte {
@@ -178,6 +183,151 @@ func BenchmarkFileLogger(b *testing.B) {
 	logger, err := newFileLogger(os.DevNull)
 	if err != nil {
 		b.Fatalf("failed to create file logger: %v", err)
+	}
+	b.Cleanup(func() {
+		logger.Close() //nolint:errcheck // Ignore error on cleanup
+	})
+
+	s := strings.Repeat("Hello World!", 1000)
+
+	b.ResetTimer()
+	for b.Loop() {
+		logger.Logf("%s", s)
+	}
+}
+
+func captureCmdLoggerOutput(t *testing.T, f func(l logger)) []byte {
+	t.Helper()
+
+	// Create a temporary file to capture the output
+	tempdir := t.TempDir()
+	tmpfile := filepath.Join(tempdir, "cmdlogger_test")
+
+	logger, err := newCmdLogger("cat > " + tmpfile)
+	if err != nil {
+		t.Fatalf("failed to create cmd logger: %v", err)
+	}
+	defer logger.Close() //nolint:errcheck // Ignore error on cleanup
+
+	// Call the function that writes to the cmd logger
+	f(logger)
+
+	ctx, cancel := context.WithTimeout(t.Context(), 5*time.Second)
+	defer cancel()
+	if err := logger.Shutdown(ctx); err != nil {
+		t.Fatalf("Shutdown() returned error: %v", err)
+	}
+
+	// Read the contents of the temporary file
+	got, err := os.ReadFile(tmpfile)
+	if err != nil {
+		t.Fatalf("failed to read from temp file: %v", err)
+	}
+
+	return got
+}
+
+func TestCmdLogger(t *testing.T) {
+	t.Run("Logf", func(t *testing.T) {
+		got := captureCmdLoggerOutput(t, func(l logger) {
+			l.Logf("Hello, %s!", "world")
+		})
+		want := "Hello, world!\n"
+		if string(got) != want {
+			t.Errorf("Logf() wrote %q, want %q", got, want)
+		}
+
+		got = captureCmdLoggerOutput(t, func(l logger) {
+			l.Logf("Hello, %s!\n", "world")
+		})
+		want = "Hello, world!\n"
+		if string(got) != want {
+			t.Errorf("Logf() wrote %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Shutdown", func(t *testing.T) {
+		// mock stderr
+		stderr := os.Stderr
+		t.Cleanup(func() {
+			os.Stderr = stderr
+		})
+		var wg sync.WaitGroup
+		var buf bytes.Buffer
+		r, w, err := os.Pipe()
+		if err != nil {
+			t.Fatalf("failed to create pipe: %v", err)
+		}
+		os.Stderr = w
+		wg.Go(func() {
+			defer r.Close()
+			if _, err := buf.ReadFrom(r); err != nil {
+				t.Errorf("failed to read from pipe: %v", err)
+			}
+		})
+
+		// build the logger
+		dir := t.TempDir()
+		loggerBinFile := filepath.Join(dir, "logger")
+		cmd := exec.Command("go", "build", "-o", loggerBinFile, filepath.Join("testdata", "logger", "logger", "main.go"))
+		if err := cmd.Run(); err != nil {
+			t.Fatalf("failed to build logger binary: %v", err)
+		}
+
+		// start the logger
+		logFile := filepath.Join(dir, "server.log")
+		logger, err := newCmdLogger(loggerBinFile + " " + logFile)
+		if err != nil {
+			t.Fatalf("failed to create cmd logger: %v", err)
+		}
+		defer logger.Close() //nolint:errcheck // Ignore error on cleanup
+
+		logger.Logf("Hello, %s!\n", "world")
+		select {
+		case <-logger.Done():
+			t.Fatalf("logger terminated unexpectedly")
+		case <-time.After(time.Second):
+		}
+
+		// shutdown
+		func() {
+			ctx, cancel := context.WithTimeout(t.Context(), 20*time.Second)
+			defer cancel()
+
+			if err := logger.Shutdown(ctx); err != nil {
+				t.Fatalf("Shutdown() returned error: %v", err)
+			}
+			w.Close() //nolint:errcheck // Ignore error on cleanup
+			wg.Wait()
+		}()
+		select {
+		case <-logger.Done():
+		case <-time.After(time.Second):
+			t.Fatalf("logger did not terminate after shutdown")
+		}
+
+		want := "logger: started\n" +
+			"logger: received EOF\n" +
+			"logger: received terminated\n"
+		if buf.String() != want {
+			t.Errorf("want %q, got %q", want, buf.String())
+		}
+
+		content, err := os.ReadFile(logFile)
+		if err != nil {
+			t.Fatalf("failed to read log file: %v", err)
+		}
+		want = "Hello, world!\n"
+		if string(content) != want {
+			t.Errorf("want %q, got %q", want, string(content))
+		}
+	})
+}
+
+func BenchmarkCmdLogger(b *testing.B) {
+	logger, err := newCmdLogger("cat > /dev/null")
+	if err != nil {
+		b.Fatalf("failed to create cmd logger: %v", err)
 	}
 	b.Cleanup(func() {
 		logger.Close() //nolint:errcheck // Ignore error on cleanup
