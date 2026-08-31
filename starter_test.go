a4752a4f8d13aa89e0a44286048bf3d05faa80b7
ICHINOSE Shogo <shogo82148@gmail.com>
2026-08-31 22:43:37 +0900

    enhance: add Test_OverrideAutoRestartByEnvDir to validate environment variable overrides for auto-restart settings

--- starter_test.go
+++ starter_test.go
@@ -719,6 +719,132 @@ func Test_EnvDir(t *testing.T) {
 	}
 }
 
+func Test_OverrideAutoRestartByEnvDir(t *testing.T) {
+	ctx := t.Context()
+	dir := t.TempDir()
+
+	// set up envdir
+	envdir := filepath.Join(dir, "envdir")
+	if err := os.Mkdir(envdir, 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	// override auto-restart settings by envdir
+	envfile := filepath.Join(envdir, EnableAutoRestartEnvName)
+	if err := os.WriteFile(envfile, []byte("1\n"), 0644); err != nil {
+		t.Fatal(err)
+	}
+	envfile = filepath.Join(envdir, KillOldDelayEnvName)
+	if err := os.WriteFile(envfile, []byte("3\n"), 0644); err != nil {
+		t.Fatal(err)
+	}
+	envfile = filepath.Join(envdir, AutoRestartIntervalEnvName)
+	if err := os.WriteFile(envfile, []byte("6\n"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// build autorestart
+	binFile := filepath.Join(dir, "autorestart")
+	cmd := exec.CommandContext(ctx, "go", "build", "-o", binFile, filepath.Join("testdata", "autorestart", "main.go"))
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Failed to compile %s: %s\n%s", dir, err, output)
+	}
+
+	statusFile := filepath.Join(dir, "status")
+	sd := &Starter{
+		Command:    binFile,
+		Ports:      []string{"0"},
+		StatusFile: statusFile,
+		EnvDir:     envdir,
+	}
+	defer sd.Close() //nolint:errcheck // ignore error on cleanup
+	go func() {
+		if err := sd.Run(); err != nil {
+			t.Errorf("sd.Run() failed: %s", err)
+		}
+	}()
+
+	time.Sleep(100 * time.Millisecond) // wait for starting worker
+
+	// connect to the first worker.
+	addr := sd.Listeners()[0].Addr().String()
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("fail to dial: %s", err)
+	}
+	if _, err := conn.Write([]byte("hello")); err != nil {
+		t.Fatalf("fail to write: %s", err)
+	}
+	var buf [1024 * 1024]byte
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		t.Fatalf("fail to read: %s", err)
+	}
+	if ok, _ := regexp.Match(`^\d+:hello$`, buf[:n]); !ok {
+		t.Errorf(`want /^\d+:hello$/, got %s`, buf[:n])
+	}
+	pid1 := string(buf[:bytes.IndexByte(buf[:], ':')])
+	conn.Close()
+
+	// new worker spawn at 7sec (since start, interval(1sec) + auto_restart_interval(6sec))
+	// status updated at 8sec (7sec + interval(1sec))
+	// old dies at 11sec (8sec + kill_old_delay(2sec) + sleep(1sec) in the child source code
+
+	// check status before auto-restart
+	time.Sleep(6 * time.Second)
+	status, err := os.ReadFile(statusFile)
+	if err != nil {
+		t.Errorf("fail to read status file %s: %s", statusFile, err)
+	}
+	if ok, _ := regexp.Match(`^1:\d+\n$`, status); !ok {
+		t.Errorf(`want /1:\d+\n/, got %s`, status)
+	}
+
+	// status during transient state
+	time.Sleep(3 * time.Second)
+	status, err = os.ReadFile(statusFile)
+	if err != nil {
+		t.Errorf("fail to read status file %s: %s", statusFile, err)
+	}
+	if ok, _ := regexp.Match(`^1:\d+\n2:\d+\n$`, status); !ok {
+		t.Errorf(`want /1:\d+\n2:\d+\n/, got %s`, status)
+	}
+
+	// status after auto-restart
+	time.Sleep(3 * time.Second)
+	status, err = os.ReadFile(statusFile)
+	if err != nil {
+		t.Errorf("fail to read status file %s: %s", statusFile, err)
+	}
+	if ok, _ := regexp.Match(`^2:\d+\n$`, status); !ok {
+		t.Errorf(`want /2:\d+\n/, got %s`, status)
+	}
+
+	// connect to the second worker.
+	conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		t.Fatalf("fail to dial: %s", err)
+	}
+	if _, err := conn.Write([]byte("hello")); err != nil {
+		t.Fatalf("fail to write: %s", err)
+	}
+	n, err = conn.Read(buf[:])
+	if err != nil {
+		t.Fatalf("fail to read: %s", err)
+	}
+	if ok, _ := regexp.Match(`^\d+:hello$`, buf[:n]); !ok {
+		t.Errorf(`want /^\d+:hello$/, got %s`, buf[:n])
+	}
+	pid2 := string(buf[:bytes.IndexByte(buf[:], ':')])
+	if pid1 == pid2 {
+		t.Errorf("want another, got %s", pid2)
+	}
+
+	if err := sd.Shutdown(ctx); err != nil {
+		t.Errorf("sd.Shutdown() failed: %s", err)
+	}
+}
+
 func TestExtractCommand(t *testing.T) {
 	tests := []struct {
 		log     string
