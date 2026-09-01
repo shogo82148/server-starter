b41e25fb44a1bf4e60a931c2e092670e7d2cdb7f
ICHINOSE Shogo <shogo82148@gmail.com>
2026-09-01 11:38:42 +0900

    add tests for UDP

---
+++ testdata/udp/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/shogo82148/server-starter/listener"
+)
+
+func main() {
+	go watchSignal()
+
+	conn, err := listener.ListenPacketAll(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn[0].Close()
+
+	for {
+		var buf [1024 * 1024]byte
+		n, addr, err := conn[0].ReadFrom(buf[:])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := conn[0].WriteTo(bytes.ToUpper(buf[:n]), addr); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func watchSignal() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGUSR1)
+	<-c
+	os.Exit(0)
+}
